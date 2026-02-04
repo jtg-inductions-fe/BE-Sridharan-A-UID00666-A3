f14ddd8498eb43b1376277b58a2070e69a30a273
Sridharan-AR1 <sridharan@joshtechnologygroup.com>
2026-02-04 18:48:38 +0530

    SA_A3_02 : Added Logout and Refresh Token views

--- bookmyshow/settings.py
+++ bookmyshow/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "apps.base",
     "apps.users",
     "apps.movies",
@@ -144,7 +145,9 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
